d05d8b71d35e3ba43fe2bd96a5b557ea12391b11
Dylan Hunn <dylhunn@gmail.com>
2017-05-13 19:44:05 -0700

    transposition table works

--- transtable/transtable.go
+++ transtable/transtable.go
@@ -1,6 +1,8 @@
 package transtable
 
-import "github.com/dylhunn/dragontoothmg"
+import (
+	"github.com/dylhunn/dragontoothmg"
+)
 
 // Transposition table entry types
 const (
@@ -8,6 +10,18 @@ const (
 	UpperBound = iota // all-node (fail-low)
 	Exact      = iota // pv-node
 )
+
+// Table data
+var keys []uint64
+var values []uint64
+
+// Initialize (or reinitialize and clear) the table. Must be called before use.
+func Initialize(sizeInMb int) {
+	bits := 8 * 1024 * 1024 * sizeInMb
+	entries := (bits / 128) + 1 // seek an odd number
+	keys = make([]uint64, entries, entries)
+	values = make([]uint64, entries, entries)
+}
 
 // Key structure, from LSB
 // eval result (16 bits)
@@ -19,8 +33,32 @@ const (
 // Insert an entry into the transposition table, which requires the board position,
 // the best move, the evaluation score, the entry search depth, and the node type.
 func Put(b *dragontoothmg.Board, m dragontoothmg.Move, eval int16, depth uint8, ntype uint8) {
-	var value uint64 = uint64(eval) & (uint64(m) << 16) & (uint64(b.Fullmoveno) << 32) &
-		(uint64(depth) << 48) & (uint64(ntype) << 56)
-	key := b.Hash() & value
-	_ = key
+	var value uint64 = uint64(uint16(eval)) | (uint64(m) << 16) | (uint64(b.Fullmoveno) << 32) |
+		(uint64(depth) << 48) | (uint64(ntype) << 56)
+	hash := b.Hash()
+	key := hash ^ value
+	index := hash % uint64(len(keys))
+	// TODO(dylhunn): Try various probing and replacement strategies
+	keys[index] = key
+	values[index] = value
+}
+
+func Get(b *dragontoothmg.Board) (found bool, move dragontoothmg.Move,
+	eval int16, depth uint8, ntype uint8) {
+	hash := b.Hash()
+	index := hash % uint64(len(keys))
+	// TODO(dylhunn): Investigate atomics. Atomics might not be necessary on
+	// 64-bit platforms, or at all, since we detect corrupted data anyway.
+	key := keys[index]
+	value := values[index]
+	found = (hash == (key ^ value))
+	if !found { // TODO(dylhunn): benchmark with and without a branch
+		// TODO(dylhunn): Count misses and invalid reads separately
+		return false, 0, 0, 0, 0
+	}
+	eval = int16((value & 0xFFFF))
+	move = dragontoothmg.Move((value >> 16) & 0xFFFF)
+	depth = uint8((value >> 48) & 0xFF)
+	ntype = uint8((value >> 56) & 0xFF)
+	return
 }
